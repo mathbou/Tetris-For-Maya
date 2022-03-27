f623eb78151c6bfa153c9ff1300119e9459ba676
Mathieu <923463-mathbou@users.noreply.gitlab.com>
2022-03-28 01:43:45 +0200

    📝 bug list

--- tetris_maya/__init__.py
+++ tetris_maya/__init__.py
@@ -3,3 +3,10 @@ from .game import Game
 
 def launch_game():
     Game.start()
+
+
+# TODO bug de queue, ne respecte pas la règle des 7
+# TODO bug de rotation quand collé a gauche
+# TODO bug de rotation quand collé en haut
+# TODO bug pas de score sur le harddrop
+# TODO bug le harddrop ne descend pas toujours jusqu'en bas
