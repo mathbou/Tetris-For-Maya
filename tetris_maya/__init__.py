f8272711ab3593369d9f2cbd3178a4e9fd8627dd
Mathieu <923463-mathbou@users.noreply.gitlab.com>
2022-03-28 22:59:17 +0200

    🐛 fix harddrop score

--- tetris_maya/__init__.py
+++ tetris_maya/__init__.py
@@ -9,5 +9,4 @@ def launch_game():
 
 # TODO bug de rotation quand collé a gauche
 # TODO bug de rotation quand collé en haut
-# TODO bug pas de score sur le harddrop
 # TODO bug le harddrop ne descend pas toujours jusqu'en bas
