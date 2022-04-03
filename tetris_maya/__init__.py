2c508807d6314f2c77d20547b613a3c68b7a4149
Mathieu <923463-mathbou@users.noreply.gitlab.com>
2022-04-03 19:38:41 +0200

    ✨ add shelf installer

--- tetris_maya/__init__.py
+++ tetris_maya/__init__.py
@@ -10,8 +10,18 @@ Warnings:
 
 ---------------------------------------------------------------------
 """
+from pathlib import Path
+
+import maya.mel as mel
+import pkg_resources
+
 from .game import Game
 
 
 def launch():
     Game.start()
+
+
+def install_shelf():
+    shelf_location = Path(pkg_resources.resource_filename("tetris_maya", "resources/shelf_Game.mel"))
+    mel.eval(f'loadNewShelf "{shelf_location.as_posix()}"')
