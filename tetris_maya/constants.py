892c00f40ee9d637815cfcb223c19578a6d9f0d9
Mathieu <923463-mathbou@users.noreply.gitlab.com>
2022-04-04 23:33:59 +0200

    ⚰ rm unused constants

--- tetris_maya/constants.py
+++ tetris_maya/constants.py
@@ -4,6 +4,3 @@ TIME_STEP = 0.5
 
 SCORE_TABLE = {1: 100, 2: 300, 3: 500, 4: 800}
 
-SCORE_HUD_NAME = f"{PREFIX}_score_hud"
-LEVEL_HUD_NAME = f"{PREFIX}_level_hud"
-LINES_HUD_NAME = f"{PREFIX}_lines_hud"
