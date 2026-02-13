9f423d9db0250c559e366a30e4c4e5c6a03cd310
Mathieu <923463-mathbou@users.noreply.gitlab.com>
2026-02-13 18:59:59 +0100

    📄 add licence headers

--- V1/tetris.py
+++ V1/tetris.py
@@ -1,17 +1,29 @@
-"""
----------------------Tetris For Maya----------------------------------
-
-
-Author: Mathieu BOUZARD
-Date : 05-2016
-Version : 1.0
-
-Warnings:
-    Only works on Maya 2016 or lower
-
-
----------------------------------------------------------------------
-"""
+# ---------------------Tetris For Maya----------------------------------
+#
+# Version : 1.0
+#
+# Warnings:
+#     Only works on Maya 2016 or lower
+#
+#
+# Copyright (c) 2016 Mathieu Bouzard.
+#
+# This file is part of Tetris Maya 
+# (see https://gitlab.com/mathbou/TetrisMaya).
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
 
 import maya.cmds as cmds
 from random import shuffle
--- V2/main.py
+++ V2/main.py
@@ -1,18 +1,31 @@
-"""
----------------------Tetris For Maya----------------------------------
+# ---------------------Tetris For Maya----------------------------------
+#
+# Version : 2.0
+#
+# Warnings:
+#     - Only work on Maya 2017-2020
+#     - This version requires a separate venv that will launch the keyboard catcher
+#
+#
+# Copyright (c) 2018 Mathieu Bouzard.
+#
+# This file is part of Tetris Maya 
+# (see https://gitlab.com/mathbou/TetrisMaya).
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
 
-
-Author: Mathieu BOUZARD
-Date : 03-2018
-Version : 2.0
-
-Warnings:
-    Only work on Maya 2017-2020
-
-This version requires a separate venv that will launch the keyboard catcher
-
----------------------------------------------------------------------
-"""
 import maya.OpenMayaUI as mui
 import shiboken2
 from PySide2 import QtWidgets
