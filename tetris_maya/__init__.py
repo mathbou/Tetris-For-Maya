394ae9c536520245208ce1be92b1467f476140a4
Mathieu <923463-mathbou@users.noreply.gitlab.com>
2026-02-13 18:59:40 +0100

    📝 add header

--- tetris_maya/__init__.py
+++ tetris_maya/__init__.py
@@ -1,3 +1,16 @@
+"""
+---------------------Tetris For Maya----------------------------------
+
+
+Author: Mathieu BOUZARD
+Date : 04-2022
+Version : 3.0.0
+
+Warnings:
+    Only work on Maya 2022+
+
+---------------------------------------------------------------------
+"""
 from .game import Game
 
 
