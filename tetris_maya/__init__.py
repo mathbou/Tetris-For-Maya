11ed18822ec21fdb7d02555c853a78b9eeb0ec43
Mathieu <923463-mathbou@users.noreply.gitlab.com>
2026-02-13 18:59:10 +0100

    🐛 fix harddrop not moving down: add substep to loopWorker

--- tetris_maya/__init__.py
+++ tetris_maya/__init__.py
@@ -3,10 +3,8 @@ from .time2 import timer_precision
 
 
 def launch_game():
-    with timer_precision():
-        Game.start()
+    Game.start()
 
 
 # TODO bug de rotation quand collé a gauche
 # TODO bug de rotation quand collé en haut
-# TODO bug le harddrop ne descend pas toujours jusqu'en bas
